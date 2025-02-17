f283e0de056aa4ed22cd9a627ba3939bd55407f5
GS <GS@T3INF-10>
2025-02-17 15:45:48 +0100

    Programa inicial, versión 1.0

---
+++ src/main/java/com/mycompany/ejemplojava2025/EjemploJava2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplojava2025;
+
+/**
+ *
+ * @author GS
+ */
+public class EjemploJava2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Gonzalo");
+    }
+}
